e45ef4fb62c35123fb0efc1dd1e81806e0f38f6a
Qiqi Wang <qiqi@mit.edu>
2015-01-11 22:22:21 -0500

    Working on nInputOperator

--- 1d/test.cpp
+++ 1d/test.cpp
@@ -1,90 +1,17 @@
-#include<vector>
+#include "test.h"
 
-class Variable {
-    private:
-
-    bool synced;
-    double * data;
-
-    public:
-};
-
-Variable operator - (Variable x) {
-    return x;
-}
-
-Variable operator + (Variable x, Variable y) {
-    return x;
-}
-
-Variable operator - (Variable x, Variable y) {
-    return x;
-}
-
-Variable operator += (Variable x, Variable y) {
-    return x;
-}
-
-Variable operator * (Variable x, Variable y) {
-    return y;
-}
-
-Variable operator * (double a, Variable x) {
-    return x;
-}
-
-class LocalVariable {
-    private:
-    double* _val;
-    std::vector<double*> _nbr;
-
-    public:
-    double val() const {
-        return *_val;
-    }
-    void val(double newVal) {
-        *_val = newVal;
-    }
-    double nbr(int i) const {
-        return *_nbr.at(i);
-    }
-};
-
-class LocalMesh {
-    public:
-    double dx;
-};
-
-typedef void (*LocalOperator)(std::vector<const LocalVariable> inputs,
-             std::vector<LocalVariable> outputs,
-             LocalMesh& mesh);
-
-class Operator {
-    public:
-    Operator (LocalOperator localOp) {
-    }
-};
-
-Operator operator >> (Operator op, Variable v) {
-    return op;
-}
-
-Operator operator << (Operator op, Variable v) {
-    return op;
-}
-
-void laplacian(std::vector<const LocalVariable> inputs,
-             std::vector<LocalVariable> outputs,
-             LocalMesh& mesh) {
+void laplacian(std::array<const LocalVariable, 1> inputs,
+               std::array<LocalVariable, 1> outputs,
+               LocalMesh& mesh) {
     double u = inputs[0].val();
     double uL = inputs[0].nbr(0);
     double uR = inputs[0].nbr(1);
     outputs[0].val((uL + uR - 2 * u) / (mesh.dx * mesh.dx));
 }
 
-void derivative(std::vector<const LocalVariable> inputs,
-             std::vector<LocalVariable> outputs,
-             LocalMesh& mesh) {
+void derivative(std::array<const LocalVariable, 1> inputs,
+                std::array<LocalVariable, 1> outputs,
+                LocalMesh& mesh) {
     double uL = inputs[0].nbr(0);
     double uR = inputs[0].nbr(1);
     outputs[0].val((uR - uL) / (2 * mesh.dx));
